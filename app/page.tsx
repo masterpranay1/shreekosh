8290bd5d719ef93f14c66d2b00383e0cca535a5a
Pranay Raj <58875281+masterpranay1@users.noreply.github.com>
2024-02-26 05:31:49 +0000

    chore(app) : stlying update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+      <h1 className="text-green-400 text-4xl">
         Shreekosh
       </h1>
     </main>
