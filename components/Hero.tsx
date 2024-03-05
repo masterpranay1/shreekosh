8af73a4fcc92ea1b578860aa25fd57a7811e0c0e
masterpranay1 <rajpranaychottu@gmail.com>
2024-03-05 23:53:47 +0530

    Refactor: Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,36 +1,57 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 import SurgeryImage from "@/public/pexels-gaspar-osorio-15771808.jpg";
+import { Sparkle } from "lucide-react";
+import { Button } from "./ui/button";
 
 const Hero = () => {
-    return (
-        <div className='relative min-h-screen my-20'>
-            <Image
-                src={SurgeryImage}
-                width={1000}
-                height={600}
-                alt="hero bg"
-                className="w-[580px] xl:w-[620px] h-auto absolute right-0 top-0 -z-10"
-            />
-            <div className='container h-[calc(100vh-120px)] gried place-items-center'>
-                <div className='space-y-4 bg-[#ffffff98] w-fit p-4'>
-                    <p className='uppercase font-medium'>Empowering Healthcare Supply Chains </p>
-                    <h3 className='text-4xl sm:text-6xl front-bold'> <span className='text-blue-400 '>24-HOUR WITH YOU</span>
-                        </h3>
-                    <p className='text-gray-700 text-[14px] sm:text-[16px] w-1/2'>Flexible cash trade options,
-                        and extensive variety of brands,
-                        we strive to exceed the expectations of our customers and partners.
-                        Join us in our journey to transform healthcare supply chains and make a positive impact on the lives of millions.
-                        Varek Pharmasol
-                        Your Trusted Healthcare Partner.</p>
-                    
-                    <button className='bg-blue-500 text-black px-6 py-2 rounded-2xl text-[14px] sm:text-[16px] '>FORM</button>
+  return (
+    <section className="p-12 lg:p-24 flex flex-row">
+      <div className="w-full md:w-1/2">
+        <h1 className="text-2xl font-extrabold leading-8 w-full md:w-8/12 md:text-4xl lg:text-5xl">
+          Revolutionizing Healthcare Supply Chain in{" "}
+          <span className="text-teal-500">Rural India</span>
+        </h1>
 
+        <ul className="my-8 text-sm md:text-base flex flex-col gap-2 text-slate-600">
+          <li className="flex flex-row items-start gap-2">
+            <Sparkle size={24} />
+            <span>Guaranteed 24-Hour Delivery</span>
+          </li>
 
-                </div>    
-            </div>
+          <li className="flex flex-row items-start gap-2">
+            <Sparkle size={24} />
+            <span>Wide Variety of Brands</span>
+          </li>
+
+          <li className="flex flex-row items-start gap-2">
+            <Sparkle size={24} />
+            <span>Flexible Cash Trade</span>
+          </li>
+        </ul>
+
+        <Button
+          className="bg-teal-400 hover:bg-teal-500 hover:-translate-y-1 text-white rounded px-8 w-full mt-8 md:w-fit"
+          size={"lg"}
+        >
+          Enquire Now
+        </Button>
+      </div>
+
+      <div className="hidden md:flex justify-center md:w-1/2 relative">
+        <Image
+          src={SurgeryImage}
+          alt="Surgery Image"
+          height={400}
+          width={600}
+          className="rounded-xl h-96 w-96 z-10"
+        />
+        <div className="hidden lg:block absolute w-96 h-96 border-4 border-teal-400 translate-x-4 translate-y-4 rounded-xl">
+
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
