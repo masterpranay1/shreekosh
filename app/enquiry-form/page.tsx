107e5735c6c1f10981e0147425d06fca9032216b
masterpranay1 <rajpranaychottu@gmail.com>
2024-03-08 20:27:52 +0530

    feat: enquiry form

--- app/enquiry-form/page.tsx
+++ app/enquiry-form/page.tsx
@@ -1,42 +1,50 @@
 "use client";
 import Header from "@/components/Header";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { DatePicker } from "@/components/ui/DatePicker";
-import { useEffect, useState } from "react";
+import { Loader2 } from "lucide-react";
+import { useState } from "react";
 
 const EnquiryForm = () => {
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e: any) => {
     const formData = e;
 
     formData.set("Date", new Date().toISOString());
 
-    if(!formData.get("Name") || !formData.get("Email") || !formData.get("Phone") || !formData.get("Query")) {
+    if (
+      !formData.get("Name") ||
+      !formData.get("Email") ||
+      !formData.get("Number") ||
+      !formData.get("Query")
+    ) {
       alert("Please fill in all the fields");
+      setLoading(false);
       return;
     }
 
     try {
-      const res = await fetch('https://script.google.com/macros/s/AKfycbx2kP4nQlJOK_anvI35u2Q22O0hjVmJ4GIpofVi5ADET-IGHgGNvYtPH5-GcNpUM94c/exec', {
-      method: 'POST',
-      body: JSON.stringify({
-        Name: formData.get("Name"),
-        Email: formData.get("Email"),
-        Phone: formData.get("Number"),
-        Query: formData.get("Query"),
-        Date: formData.get("Date"),
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-
-    console.log(res);
-    const data = await res.json();
-    console.log(data);
-
-    } catch (err : any) {
+      const res = await fetch(
+        "https://script.google.com/macros/s/AKfycbx2kP4nQlJOK_anvI35u2Q22O0hjVmJ4GIpofVi5ADET-IGHgGNvYtPH5-GcNpUM94c/exec",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            Name: formData.get("Name"),
+            Email: formData.get("Email"),
+            Number: formData.get("Number"),
+            Query: formData.get("Query"),
+            Date: formData.get("Date"),
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+          mode: "no-cors",
+        }
+      );
+      setLoading(false);
+    } catch (err: any) {
       alert("There was an error submitting the form. Please try again later.");
     }
   };
@@ -50,29 +58,36 @@ const EnquiryForm = () => {
         type="text"
         placeholder="Name"
         name="Name"
-        className="p-2 md:p-4 my-2 bg-white border-0 rounded"
+        className="p-4 md:p-8 text-lg my-2 bg-white border-0 rounded placeholder:text-slate-400"
       />
       <Input
         type="email"
         placeholder="Email"
         name="Email"
-        className="p-2 md:p-4 my-2 bg-white border-0 rounded"
+        className="p-4 md:px-8  my-2 text-lg bg-white border-0 rounded placeholder:text-slate-400"
       />
       <Input
         type="text"
         placeholder="Phone"
-        name="Phone"
-        className="p-2 md:p-4 my-2 bg-white border-0 rounded"
+        name="Number"
+        className="p-4 md:px-8  my-2 text-lg bg-white border-0 rounded placeholder:text-slate-400"
       />
       <Textarea
         placeholder="Query"
         name="Query"
-        className="p-2 md:p-4 my-2 bg-white border-0 rounded"
+        className="p-4 md:px-8  my-2 text-lg bg-white border-0 rounded placeholder:text-slate-400"
       />
 
-      <button type="submit" className="bg-teal-400 text-white p-2 mt-8 rounded">
-        Submit
-      </button>
+      <Button
+        type="submit"
+        className="bg-teal-400 hover:bg-teal-500 hover:-translate-y-1 text-white rounded px-8 w-full mt-8"
+        size={"lg"}
+        onClick={() => {
+          setLoading(true);
+        }}
+      >
+        {loading ? <Loader2 size={24} className="animate-spin"/> : "Submit"}
+      </Button>
     </form>
   );
 };
