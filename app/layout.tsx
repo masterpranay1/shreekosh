e6002ecdc98515dac592ca84223e40fb7f38a6bf
masterpranay1 <rajpranaychottu@gmail.com>
2024-04-01 13:22:45 +0530

    chore: move Header to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Shreekosh - Home",
-  description: "One stop solution of multiple range of healthcare supplies for all pharmacies and nursing homes situated in rural areas",
+  description:
+    "One stop solution of multiple range of healthcare supplies for all pharmacies and nursing homes situated in rural areas",
 };
 
 export default function RootLayout({
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>{children}</body>
+      <body className={montserrat.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import USPCards from "@/components/USPCards";
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col">
-      <Header />
+      {/* <Header /> */}
 
       <section className="bg-gray-100 min-h-screen">
         <Hero />
