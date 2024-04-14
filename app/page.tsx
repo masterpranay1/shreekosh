c265b2199660a7e5ac6eaf65b4ebd9966d714982
masterpranay1 <rajpranaychottu@gmail.com>
2024-04-14 20:47:00 +0530

    refactor: home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Vision from "@/components/Vision";
 import About from "@/components/About";
 import USPTitle from "@/components/USPTitle";
 import USPCards from "@/components/USPCards";
+import CompanyList from "./components/CompanyList";
 
 export default function Home() {
   return (
@@ -15,27 +16,11 @@ export default function Home() {
         <Hero />
       </section>
 
-      {/* Mission and Vision */}
-      <section
-        className="min-h-screen flex flex-col gap-8"
-        id="mission-and-vision"
-      >
-        <Vision />
-        <Mission />
-      </section>
-
-      {/* Our USP */}
-      <section
-        className="min-h-screen bg-gray-100 flex flex-col gap-8 pt-8"
-        id="our-usp"
-      >
-        <USPTitle />
-        <USPCards />
-      </section>
-
-      {/*About us */}
-      <section className="min-h-screen flex flex-col" id="about">
-        <About />
+      <section className="mx-4 lg:mx-24 xl:mx-32 flex flex-col gap-8 md:gap-12 lg:gap-16 xl:gap-20 py-16 md:py-20 lg:py-24" id="companies">
+        <h2 className="text-4xl lg:text-5xl xl:text-6xl text-center font-extrabold text-teal-700 max-w-md lg:max-w-lg xl:max-w-2xl mx-auto">
+          Trusted by <span className="text-primary">Leading</span> companies
+        </h2>
+        <CompanyList />
       </section>
     </main>
   );
